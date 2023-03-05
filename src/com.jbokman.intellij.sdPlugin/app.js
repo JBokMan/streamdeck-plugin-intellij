3cd81497e8fc2c73e5b69a3b445bf335d74d5795
Julian Bokelmann <j.b.bokelmann+github@gmail.com>
2023-03-05 09:47:58 +0100

    ✨ send a message via a WebSocket

--- src/com.jbokman.intellij.sdPlugin/app.js
+++ src/com.jbokman.intellij.sdPlugin/app.js
@@ -1,6 +1,5 @@
 /// <reference path="libs/js/action.js" />
 /// <reference path="libs/js/stream-deck.js" />
-
 const myAction = new Action('com.jbokman.intellij.action');
 
 /**
@@ -11,7 +10,24 @@ $SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) =
 });
 
 myAction.onKeyUp(({ action, context, device, event, payload }) => {
-	console.log('Your key code goes here!');
+	const socket = new WebSocket('ws://127.0.0.1:12345');
+
+	socket.addEventListener('open', event => {
+		console.log('Socket connection established');
+		socket.send("Hello Server!")
+	});
+
+	socket.addEventListener('message', event => {
+		console.log(`Received message: ${event.data}`);
+	});
+
+	socket.addEventListener('error', event => {
+		console.error('Socket error:', event.error);
+	});
+
+	socket.addEventListener('close', event => {
+		console.log('Socket connection closed');
+	});
 });
 
 myAction.onDialRotate(({ action, context, device, event, payload }) => {
