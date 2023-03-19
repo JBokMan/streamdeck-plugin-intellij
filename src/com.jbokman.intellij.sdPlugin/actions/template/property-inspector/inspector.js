920368085de51c42d458f9966756c3a528e96825
Julian Bokelmann <j.b.bokelmann+github@gmail.com>
2023-03-19 14:00:22 +0100

    🔥 remove unused code

--- src/com.jbokman.intellij.sdPlugin/actions/template/property-inspector/inspector.js
+++ src/com.jbokman.intellij.sdPlugin/actions/template/property-inspector/inspector.js
@@ -1,12 +1,10 @@
 /// <reference path="../../../libs/js/property-inspector.js" />
 /// <reference path="../../../libs/js/utils.js" />
 
-const checkmark = document.getElementById('green-check-icon')
-
 $PI.onConnected((jsn) => {
     const form = document.querySelector('#property-inspector');
-    const {actionInfo, appInfo, connection, messageType, port, uuid} = jsn;
-    const {payload, context} = actionInfo;
+    const {actionInfo} = jsn;
+    const {payload} = actionInfo;
     const {settings} = payload;
 
     Utils.setFormValue(settings, form);
@@ -39,9 +37,7 @@ document.querySelector('#open-external').addEventListener('click', () => {
 
     const socket = new WebSocket(`ws://${username}:${password}@127.0.0.1:12345/testConnection`);
 
-    // TODO check if error or success and show a symbol in the stream deck view
-
-    socket.addEventListener('open', event => {
+    socket.addEventListener('open', _ => {
         console.log('Socket connection established');
         connected = true
         socket.send("ping")
@@ -62,7 +58,7 @@ document.querySelector('#open-external').addEventListener('click', () => {
         document.getElementById("red-x-icon").style.visibility = "visible";
     });
 
-    socket.addEventListener('close', event => {
+    socket.addEventListener('close', _ => {
         console.log('Socket connection closed');
         document.getElementById("green-check-icon").style.visibility = "hidden";
         document.getElementById("red-x-icon").style.visibility = "visible";
@@ -74,64 +70,3 @@ document.querySelector('#open-external').addEventListener('click', () => {
         connected = false
     })
 });
-
-
-/**
- * TABS
- * ----
- *
- * This will make the tabs interactive:
- * - clicking on a tab will make it active
- * - clicking on a tab will show the corresponding content
- * - clicking on a tab will hide the content of all other tabs
- * - a tab must have the class "tab"
- * - a tab must have a data-target attribute that points to the id of the content
- * - the content must have the class "tab-content"
- * - the content must have an id that matches the data-target attribute of the tab
- *
- *  <div class="tab selected" data-target="#tab1" title="Show some inputs">Inputs</div>
- *  <div class="tab" data-target="#tab2" title="Here's some text-areas">Text</div>
- * a complete tab-example can be found in the index.html
- <div type="tabs" class="sdpi-item">
- <div class="sdpi-item-label empty"></div>
- <div class="tabs">
- <div class="tab selected" data-target="#tab1" title="Show some inputs">Inputs</div>
- <div class="tab" data-target="#tab2" title="Here's some text-areas">Text</div>
- </div>
- </div>
- <hr class="tab-separator" />
- * You can use the code below to activate the tabs (`activateTabs` and `clickTab` are required)
- */
-
-function activateTabs(activeTab) {
-    const allTabs = Array.from(document.querySelectorAll('.tab'));
-    let activeTabEl = null;
-    allTabs.forEach((el, i) => {
-        el.onclick = () => clickTab(el);
-        if (el.dataset?.target === activeTab) {
-            activeTabEl = el;
-        }
-    });
-    if (activeTabEl) {
-        clickTab(activeTabEl);
-    } else if (allTabs.length) {
-        clickTab(allTabs[0]);
-    }
-}
-
-function clickTab(clickedTab) {
-    const allTabs = Array.from(document.querySelectorAll('.tab'));
-    allTabs.forEach((el, i) => el.classList.remove('selected'));
-    clickedTab.classList.add('selected');
-    activeTab = clickedTab.dataset?.target;
-    allTabs.forEach((el, i) => {
-        if (el.dataset.target) {
-            const t = document.querySelector(el.dataset.target);
-            if (t) {
-                t.style.display = el == clickedTab ? 'block' : 'none';
-            }
-        }
-    });
-}
-
-activateTabs();
